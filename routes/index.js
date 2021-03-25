d6159ce5d92728abd688f75c9cf1fed004ede18c
Kateti Mareko <58158244+kateti-mareko@users.noreply.github.com>
2021-03-26 12:09:06 +1300

    Add group router to the router

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const homeRouter = require('./home')
+const groupRouter = require('./group')
+
 
 const router = express.Router()
 
 router.use(homeRouter)
+router.use(groupRouter)
 
 module.exports = router
