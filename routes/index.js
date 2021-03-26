737456b783957bbde624e1e4d926eb9c350d44e7
Kateti Mareko <58158244+kateti-mareko@users.noreply.github.com>
2021-03-26 13:07:27 +1300

    Added to do list item feature

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const homeRouter = require('./home')
 const groupRouter = require('./group')
-
+const todoRouter = require('./todolist')
 
 const router = express.Router()
 
 router.use(homeRouter)
+router.use(todoRouter)
 router.use(groupRouter)
 
 module.exports = router
--- routes/todolist.js
+++ routes/todolist.js
@@ -1,8 +1,22 @@
 const express = require('express')
 const router = express.Router()
+const { addToDoList } = require('../controller/to-do-list-controller')
 
-router.get('/todolist/:id', (req, res) => {
+/*router.get('/todolist/:id', (req, res) => {
   res.send('hehelo')
+})*/
+
+router.post('/todoitems/:id/add', (req, res) => {
+  const data = req.body
+  const groupId = req.params.id
+  
+  addToDoList(groupId, data, (err, contents) => {
+    if (err) {
+      res.status(500).send(err)
+    } else {
+      
+    }
+  })
 })
 
 module.exports = router
